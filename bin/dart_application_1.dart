cf0e4819d7dcd556f7f3e580a83a8baa9c58f4e9
silvalfare <silvalfare@gmail.com>
2025-05-18 21:13:38 +0700

    soal 4

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -3,5 +3,6 @@ import 'package:dart_application_1/dart_application_1.dart';
 void main() {
   //soal1();
   // soal2();
-  soal3();
+  // soal3();
+  soal4();
 }
--- lib/dart_application_1.dart
+++ lib/dart_application_1.dart
@@ -1,10 +1,10 @@
 import 'dart:io';
 
-// void soal1() {
-//   for (int ganjil = 1; ganjil <= 20; ganjil += 2) {
-//     print(ganjil);
-//   }
-// }
+void soal1() {
+  for (int ganjil = 1; ganjil <= 20; ganjil += 2) {
+    print(ganjil);
+  }
+}
 
 void soal2() {
   for (int asterisk = 1; asterisk <= 5; asterisk++) {
@@ -17,5 +17,13 @@ void soal3() {
   while (nama <= 4) {
     print("Alfarezhi");
     nama++;
+  }
+}
+
+void soal4() {
+  List<String> buah = ['Apel', 'Jeruk', 'Mangga', 'Anggur'];
+
+  for (var b in buah) {
+    print('Saya suka $b');
   }
 }
